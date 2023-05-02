67772430625b9033481a444224ba04ffa431c25e
Tobias Kunze <r@rixx.de>
2023-05-02 21:43:12 +0200

    Show covers

--- src/static/private.js
+++ src/static/private.js
@@ -7,4 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
             allowEmptyOption: !select.hasAttribute('required'),
         });
     })
+    // if #wizard-select exists, add images
+    if (document.getElementById('wizard-edition')) {
+        // for each label, get the input value and prepend the image to the label
+        document.querySelectorAll('#id_edition-edition_selection label').forEach(label => {
+            const input = label.querySelector('input');
+            const img = document.createElement('img');
+            img.src = `https://covers.openlibrary.org/b/olid/${input.value}-S.jpg`;
+            img.alt = input.value;
+            label.prepend(img);
+        })
+    }
 });
