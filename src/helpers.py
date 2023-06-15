09c6f2db36d57c2ebba52e0887c9807714668a2f
Tobias Kunze <r@rixx.de>
2023-06-16 00:30:36 +0200

    Improve helper scripts

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,4 @@
+from scriptorium.main.models import *
 
 def new_tag(tag, queryset=None):
     queryset = queryset or Book.objects.all().exclude(tags__id__in=[tag.pk])
@@ -7,9 +8,14 @@ def new_tag(tag, queryset=None):
             book.tags.add(tag)
             print(f"Added tag {tag.name} to {book.title} by {book.author}")
 
-def new_author_tag(tag):
+def new_author_tag(tag, alt_tag=None):
     queryset = Author.objects.all()
     for author in queryset:
         print(f"Tag {author.name} with {tag.name} tag? (y/n)")
         if input().lower() == "y":
             author.tag_author(tag)
+        elif alt_tag:
+            author.tag_author(alt_tag)
+
+# create thumbnails if none exist
+# create colour if none exists
