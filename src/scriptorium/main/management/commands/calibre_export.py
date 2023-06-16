0ddfc9cc815171957bf435771a839bcede5f3ba9
Tobias Kunze <r@rixx.de>
2023-06-16 17:02:07 +0200

    Make calibre-import two-part

---
+++ src/scriptorium/main/management/commands/calibre_export.py
@@ -0,0 +1,19 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def handle(self, *args, **options):
+        query = "'tags:\"=on-device\"'"
+        result = subprocess.check_output(
+            f"calibredb list -s {query} --fields authors,title,*pages,*shelf --for-machine",
+            shell=True,
+            env={},
+        ).decode()
+        with open("calibre_books.json", "w") as f:
+            # Round-trip through json to make sure it's valid
+            json.dump(json.loads(result), f)
+            print("Wrote calibre_books.json")
---
+++ src/scriptorium/main/management/commands/calibre_import.py
@@ -0,0 +1,41 @@
+import json
+import subprocess
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from scriptorium.main.models import ToRead
+
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def add_arguments(self, parser):
+        parser.add_argument("json_file")
+
+    def handle(self, *args, json_file, **options):
+        with open(json_file) as f:
+            result = json.load(f)
+        calibre_books = {(b["title"], b["authors"]): b for b in result}
+        scriptorium_books = {(b.title, b.author): b.id for b in ToRead.objects.all()}
+        unknown = set(calibre_books) - set(scriptorium_books)
+        too_many = set(scriptorium_books) - set(calibre_books)
+
+        with transaction.atomic():
+            ToRead.objects.filter(
+                id__in=(v for k, v in scriptorium_books.items() if k in too_many)
+            ).delete()
+            ToRead.objects.bulk_create(
+                [
+                    ToRead(
+                        title=b["title"],
+                        author=b["authors"],
+                        shelf=b["*shelf"],
+                        pages=b.get("*pages", 0),
+                        source="calibre",
+                    )
+                    for k, b in calibre_books.items()
+                    if k in unknown
+                ],
+                ignore_conflicts=True,
+            )
